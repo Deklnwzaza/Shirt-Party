6b51cb358088f249e01122ffd7c12827c83b78db
Deklnwzaza <dietyboy@hotmail.com>
2017-05-10 03:49:44 +0700

    init route for api and web

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('user/create', 'UserController@createUser');
+
+Route::post('user/login', 'UserAuthController@loginUser');
+
+Route::post('shirt/create', 'StripedShirtController@createShirt');
+
+Route::post('sticker/create', 'StickerController@createSticker');
+
+Route::get('shirt/all', 'StripedShirtController@getAllShirt');
+
+Route::get('shirt/{id}', 'StripedShirtController@getShirt');
+
+Route::get('sticker/all', 'StickerController@getAllSticker');
+
+Route::get('sticker/{id}', 'StickerController@getSticker');
